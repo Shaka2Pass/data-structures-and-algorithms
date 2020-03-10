9e5a0b33658b743ac190da65485b31db93033339
Will G Piro <willgpiro@gmail.com>
2020-03-09 21:46:51 -0700

    added pseudo code from notebook

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,9 +1,10 @@
 //Write a function called map which takes an array and a function as arguments. Without utilizing any of the built-in methods available to your language, return an array with the function applied to each element.
 
-
+// map will take an array and a callback
 const map = (arr, callback) => {
+    // set the elements modified by the callback into a new array.
     const newArray = [];
-
+    //transform each element of the arrary by calling the callback function passing the element and the current index.
     for (let i = 0; i < arr.length; i++) {
         newArray[i] = callback(arr[i]);  
     }
@@ -11,6 +12,8 @@ const map = (arr, callback) => {
     return newArray;
 };
 
+//test the function. Should return [3, 6, 9, 12]
+console.log(map([1,2,3,4], num => num * 3)
 
 
 
